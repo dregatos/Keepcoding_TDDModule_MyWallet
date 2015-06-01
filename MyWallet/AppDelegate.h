901d3f559e921e280f6e4685fa55b1efd678b99f
David Regatos <david.regatos@gmail.com>
2015-06-01 22:33:20 +0200

    Added .gitignore

---
+++ MyWallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWallet
+//
+//  Created by David Regatos on 01/06/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
